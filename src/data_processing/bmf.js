261c59c7c73fca4901e46d8f305c927c1934e8a9
github username <chyrellelewis@gmail.com>
2019-09-08 09:08:06 -0500

    json bmp with values

--- src/data_processing/bmf.js
+++ src/data_processing/bmf.js
@@ -36,7 +36,7 @@ let csvToJson = require('convert-csv-to-json');
 // 
 
  
-let json = csvToJson.getJsonFromCsv("altere_data.csv");
+let json = csvToJson.formatValueByType().getJsonFromCsv("altere_data.csv");
 for(let i=0; i<json.length;i++){
     console.log(json[i]);
 }
