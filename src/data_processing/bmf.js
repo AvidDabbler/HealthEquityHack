2dfde26f0274576fb35f89c521aa925b0aa0038f
github username <chyrellelewis@gmail.com>
2019-09-08 09:04:22 -0500

    json bmf

--- src/data_processing/bmf.js
+++ src/data_processing/bmf.js
@@ -1,18 +1,44 @@
-    const csv = require ('csv-parser')
-    const fs = require('fs');
-    const http = require ("https");
-    const file = fs.createWriteStream("all.csv");
-    require ('./bmf.js')
+// const csvparser = require ('csv-parser')
+const fs = require('fs');
+const http = require ("https");
+const file = fs.createWriteStream("all.csv");
+const results = [];
+let csvToJson = require('convert-csv-to-json');
+// require ('../bmf.js')
+// require ('../altere_data.csv')
+// var csv = require("csvtojson");
 
 
-  http.get("https://slalom-hackathon.s3.us-east-2.amazonaws.com/MO+BMF+6.10.2019.csv", response => {
-  response.pipe(file).on('data', (row) => {
-    console.log(row); 
-  })
-  .on('end', () => {
-    console.log('CSV file successfully processed');
-    
-  });
-});
+//   http.get("https://slalom-hackathon.s3.us-east-2.amazonaws.com/MO+BMF+6.10.2019.csv", response => {
+//   response.pipe(file).on('data', (row) => {
+//     console.log(row); 
+//   })
+//   .on('finish', (data) => {
+//     console.log('CSV file successfully processed');
+
+//   });
+// });
 
 
+
+//Use async / await
+// csv()
+//   .fromFile("./all.csv")
+//   .then(function(jsonArrayObj){ //when parse finished, result will be emitted here.
+//      console.log(jsonArrayObj); 
+//      console.log(util.inspect(jsonArrayObj, {showHidden: false, depth: null}));
+//    })
+
+// const util = require('util')
+
+// console.log(util.inspect(jsonArray, {showHidden: false, depth: null}))
+
+// 
+
+ 
+let json = csvToJson.getJsonFromCsv("altere_data.csv");
+for(let i=0; i<json.length;i++){
+    console.log(json[i]);
+}
+csvToJson.fieldDelimiter('~') .getJsonFromCsv("altere_data.csv");
+csvToJson.formatValueByType().getJsonFromCsv("altere_data.csv");
